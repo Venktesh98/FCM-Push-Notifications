d962b55fe4564c8cfc865ca06452b1a879fd2007
Venktesh Soma <venktesh.soma@revinate.com>
2024-02-16 12:24:03 +0530

    added the installer banner

--- src/components/InstallerBanner.jsx
+++ src/components/InstallerBanner.jsx
@@ -1,75 +1,43 @@
-// var deferredPrompt;
-
-// window.addEventListener("beforeinstallprompt", function (e) {
-//   console.log("beforeinstallprompt Event fired");
-//   e.preventDefault();
-
-//   // Stash the event so it can be triggered later.
-//   deferredPrompt = e;
-
-//   return false;
-// });
-
-// btnSave.addEventListener("click", function () {
-//   if (deferredPrompt !== undefined) {
-//     // The user has had a postive interaction with our app and Chrome
-//     // has tried to prompt previously, so let's show the prompt.
-//     deferredPrompt.prompt();
-
-//     // Follow what the user has done with the prompt.
-//     deferredPrompt.userChoice.then(function (choiceResult) {
-//       console.log(choiceResult.outcome);
-
-//       if (choiceResult.outcome == "dismissed") {
-//         console.log("User cancelled home screen install");
-//       } else {
-//         console.log("User added to home screen");
-//       }
-
-//       // We no longer need the prompt.  Clear it up.
-//       deferredPrompt = null;
-//     });
-//   }
-// });
-
-import React from "react";
+import React, { useEffect } from "react";
 
 const InstallerBanner = () => {
-  var deferredPrompt;
+  let deferredPrompt;
 
-  if (typeof window === "undefined") {
-    window.addEventListener("beforeinstallprompt", function (e) {
+  useEffect(() => {
+    const handleBeforeInstallPrompt = (e) => {
       console.log("beforeinstallprompt Event fired");
       e.preventDefault();
-
-      // Stash the event so it can be triggered later.
       deferredPrompt = e;
+    };
 
-      return false;
-    });
-  }
+    window.addEventListener("beforeinstallprompt", handleBeforeInstallPrompt);
+
+    return () => {
+      window.removeEventListener(
+        "beforeinstallprompt",
+        handleBeforeInstallPrompt
+      );
+    };
+  }, []); // Ensure the event listener is added only once on mount
 
   const handleInstallBanner = () => {
-    if (deferredPrompt !== undefined) {
-      // The user has had a postive interaction with our app and Chrome
-      // has tried to prompt previously, so let's show the prompt.
+    if (typeof window !== "undefined" && deferredPrompt !== undefined) {
       deferredPrompt.prompt();
 
-      // Follow what the user has done with the prompt.
-      deferredPrompt.userChoice.then(function (choiceResult) {
+      deferredPrompt.userChoice.then((choiceResult) => {
         console.log(choiceResult.outcome);
 
-        if (choiceResult.outcome == "dismissed") {
+        if (choiceResult.outcome === "dismissed") {
           console.log("User cancelled home screen install");
         } else {
           console.log("User added to home screen");
         }
 
-        // We no longer need the prompt.  Clear it up.
         deferredPrompt = null;
       });
     }
   };
+
   return (
     <div>
       <button onClick={handleInstallBanner}>Click for the banner</button>
